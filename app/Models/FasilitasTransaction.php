0c339134428d14b5189dcc1bb94e1d8ccfdcd4b9
Henry Augusta <henryaugusta4@gmail.com>
2025-02-03 16:37:08 +0700

    FasilitasTransactionModel

---
+++ app/Models/FasilitasTransaction.php
@@ -0,0 +1,29 @@
+<?php
+// app/Models/FasilitasTransaction.php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class FasilitasTransaction extends Model
+{
+    use HasFactory;
+    // use SoftDeletes; // Enable soft deletes
+
+    // Define the table associated with this model
+    protected $table = 'fasilitas_transactions';
+
+    // Define fillable columns for mass assignment
+    protected $fillable = [
+        'parent_id', // Plain column, not a foreign key
+        'fasilitas_id',   // Foreign key to fasilitas table
+    ];
+
+    // Define relationship with Fasilitas
+    public function fasilitas()
+    {
+        return $this->belongsTo(Fasilitas::class, 'fasilitas_id');
+    }
+}
