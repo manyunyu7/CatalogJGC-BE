cc2f776d47d5f606dae2ab7112de727dcb82b8eb
Henry Augusta <henryaugusta4@gmail.com>
2025-02-28 10:39:52 +0700

    Add FullImgPath appendable to MySlider

--- app/Models/MySlider.php
+++ app/Models/MySlider.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class MySlider extends Model
 {
     use HasFactory;
+
+    protected $appends = ["full_img_path"]; // Fix the property name
+
+    public function getFullImgPathAttribute()
+    {
+        return url('/') . '/' . ltrim($this->image, '/'); // Ensure correct URL format
+    }
 }
